f0cbe0c8bb24a9ed04d6931b856691ca0dd3dbcd
nailashaheen-git-elastic <nailashaheen@elastic.io>
2026-08-13 11:13:36 +0200

    Add upsert pet action with pet schema

--- src/actions/upsertObject.ts
+++ src/actions/upsertObject.ts
@@ -1,47 +1,49 @@
 import { messages } from 'elasticio-node';
 import Client from '../client';
-import usersSchema from '../schemas/actions/users.json';
-import productSchema from '../schemas/actions/products.json';
+import petSchema from '../schemas/actions/pet.json';
 
 export async function processAction(msg: any, cfg: any) {
-  this.logger.info('"Upsert Object" action started');
-  const { id } = msg.body;
-  const { objectType } = cfg;
-  const reqData = JSON.parse(JSON.stringify(msg.body));
-  delete reqData.id;
-
-  const client = new Client(this, cfg);
-  if (id) {
-    try {
-      const { data } = await client.apiRequest({
-        url: `/${objectType}/${id}`,
-        method: 'PUT',
-        data: reqData
-      });
-      this.logger.info('"Upsert Object" action is done emitting...');
-      return messages.newMessageWithBody(data);
-    } catch (error) {
-      if (error.response?.status === 404) {
-        throw new Error(`Object with id "${id}" is not found!`);
-      }
-      throw error;
+    this.logger.info('"Upsert Object" action started');
+    const { id } = msg.body;
+    const { objectType } = cfg;
+    const reqData = JSON.parse(JSON.stringify(msg.body));
+    delete reqData.id;
+  
+    const client = new Client(this, cfg);
+  
+    if (objectType !== 'pet') {
+      throw new Error(`Object type "${objectType}" is not supported!`);
     }
+  
+    if (id) {
+      try {
+        const { data } = await client.apiRequest({
+          url: '/pet',
+          method: 'PUT',
+          data: { ...reqData, id: Number(id) },
+        });
+        this.logger.info('"Upsert Object" action is done emitting...');
+        return messages.newMessageWithBody(data);
+      } catch (error) {
+        if (error.response?.status === 404) {
+          throw new Error(`Object with id "${id}" is not found!`);
+        }
+        throw error;
+      }
+    }
+  
+    this.logger.info('object not found, going to create...');
+    const { data } = await client.apiRequest({
+      url: '/pet',
+      method: 'POST',
+      data: reqData,
+    });
+    this.logger.info('"Upsert Object" action is done emitting...');
+    return messages.newMessageWithBody(data);
   }
-  this.logger.info('object not found, going to create...');
-  // if object can be created with 'id' from msg.body - it should be created with this 'id'
-  const { data } = await client.apiRequest({
-    url: `/${objectType}`,
-    method: 'POST',
-    data: reqData
-  });
-  this.logger.info('"Upsert Object" action is done emitting...');
-  return messages.newMessageWithBody(data);
-}
-
 export const getMetaModel = async function (cfg: { objectType: string }) {
   const schemas = {
-    users: usersSchema,
-    products: productSchema
+    pet: petSchema,
   };
   const metadata = {
     in: {
@@ -50,9 +52,9 @@ export const getMetaModel = async function (cfg: { objectType: string }) {
         ...schemas[cfg.objectType].in,
         id: {
           type: 'string',
-          title: 'ID'
-        }
-      }
+          title: 'ID',
+        },
+      },
     },
     out: {
       type: 'object',
@@ -60,10 +62,10 @@ export const getMetaModel = async function (cfg: { objectType: string }) {
         ...schemas[cfg.objectType].out,
         id: {
           type: 'string',
-          required: true
-        }
-      }
-    }
+          required: true,
+        },
+      },
+    },
   };
   return metadata;
 };
